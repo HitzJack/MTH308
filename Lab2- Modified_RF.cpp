eb92f1e08dafe4b94e266902b8d6b11b81cc7a45
Saaumitra Raaj <130910893+HitzJack@users.noreply.github.com>
2025-01-31 16:17:22 +0530

    Update Lab2- Modified_RF.cpp

--- Lab2- Modified_RF.cpp
+++ Lab2- Modified_RF.cpp
@@ -10,68 +10,54 @@ const int MOD = 1e9 + 7;
 mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
 
 double epsilon_max = 1e-5;
-#ifndef ONLINE_JUDGE
-#include "/Users/saaumitraraaj/Desktop/cp/debugtemp.cpp"
-#else
-#define debug(...)
-#define debugArr(...)
-#endif
+
  
 void Solve(){
     
     // returns the value of function at a given point x;
     auto f = [&](double x) -> double{
         double v = 0.0;
-        // v += 2 * x * x * x - 2.5 * x - 5;
-        double pp = (98 * 1.0) / x;
-        v += 35 - (9.8 * x * (1 - pow(M_E, -pp))) / 14.0;
+        v += 2 * x * x * x - 2.5 * x - 5;
+        // double pp = (98 * 1.0) / x;
+        // v += 35 - (9.8 * x * (1 - pow(M_E, -pp))) / 14.0;
         return v;
     };
-    double xl = 0.0; // lower bound 
-    double xu = 100.0; // upper bound
+    double a = 1.0; // lower bound 
+    double b = 2.0; // upper bound
     int il = 1;
     int iu = 1;
     double iterations = 0; // to keep count of number of iterations
-    double xm_old = -1; // to keep track the last value of m ie xm_old
+    double w0 = b; // to keep track the last value of m ie xm_old
     double ea = 100; // the absolute relative error
-    double F = f(xl);
-    double G = f(xu);
-    vector < array < double , 6 >> TABLE; // to store all the needed values to make a table
-    while(abs(f(xm_old)) > epsilon_max){ // while loop breaks when number of iterations exceed 10
-        double xm = (G * xl - F * xu) / (G - F);
-        debug(xl , xu);
-        double z = f(xl) * f(xm); // z checks the condition to whether switch xl to xm or xm to xl;
-        double xm_new = xm;
-        if(xm_old == -1){
-            TABLE.push_back({iterations , xl , xu , xm , -1 , f(xm)});
-            xm_old = xm_new;
-        }
-        else{
-            ea = abs(xm_new - xm_old);
-            ea /= abs(xm_new);
-            ea *= 100; // ea represents absolute relative approximate error ie ea = abs(xm_old - xm_new) / abs(xm_new) * 100
-            TABLE.push_back({iterations , xl , xu , xm , ea , f(xm)});
-            xm_old = xm_new;
-        }
-        if(z > 0){ // if z > 0 i.e f(xl) * f(xm) > 0 i.e xl && xm are either both below x axis or above x axis hence we can use xl to be xm now to shorten our bound
-            xl = xm;
-            F = f(xl);
-            il = 0;
+    double F = f(a);
+    double G = f(b);
+    vector < array < double , 5 >> TABLE; // to store all the needed values to make a table
+    while(abs(f(w0)) > epsilon_max){ // while loop breaks when number of iterations exceed 10
+        double wn = (G * a - F * b) / (G - F);
+        double z = f(wn) * f(a); // z checks the condition to whether switch xl to xm or xm to xl;
+        TABLE.push_back({iterations , a , b , wn , f(wn)});
+        w0 = wn;
+        if(z > 0){ // if z > 0 i.e f(wn) * f(a) > 0 i.e a && wn are either both below x axis or above x axis hence we can use a to be wn now to shorten our bound
+            a = wn;
+            F = f(a); // Updating the value of the lower bound as lowe bound is changed to wn;
+            il = 0;     /* Since lower_bound is changed il = 0 && iu is incremented by 1 */
             iu++;
             if(iu > 1){
                 G /= 2.0;
             }
         }
         else{
-            // f(xl) * f(xm) < 0 i.e xl and xm are on different sides of x - axis hence xu = xm to shorten our bound
-            xu = xm;
-            G = f(xu);
+            // f(a) * f(wn) < 0 i.e a and wn are on different sides of x - axis hence b = wn to shorten our bound
+            
+            b = wn; // Updating the value of the lower bound as lowe bound is changed to wn;
+            G = f(b);   /* Since lower_bound is changed il = 0 && iu is incremented by 1 */
             iu = 0;
             il++;
             if(il > 1){
                 F /= 2.0;
             }
         }
+        iterations++;
     }
     for(auto &x : TABLE){
         int p = 0;
