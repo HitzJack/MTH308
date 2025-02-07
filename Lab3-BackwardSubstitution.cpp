bd745aba24d396febbab17f42c51750df2c5b886
Saaumitra Raaj <130910893+HitzJack@users.noreply.github.com>
2025-02-07 16:43:48 +0530

    Add files via upload

---
+++ Lab3-BackwardSubstitution.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define INF (int)1e18
+#define fi first
+#define se second
+#define pii pair<int,int>
+const int MOD = 1e9 + 7;
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+
+void Solve()
+{
+    int n = 3;
+    vector <vector <double>> A(n , vector <double>(n));
+    for(auto &x : A){
+    	for(auto &y : x){
+    		cin >> y;	
+    	}
+    }
+    vector <double> B(n);
+    for(auto &x : B) cin >> x;
+    
+    vector <double> X(n);
+	for(int i = n - 1 ; i >= 0 ; i--){
+		double tmp = B[i] * 1.0;
+		for(int j = i + 1 ; j < n ; j++){
+			tmp -= A[i][j] * X[j];
+		}
+		X[i] = tmp / A[i][i];
+	}
+	for(auto &x : X){
+		cout << x << " ";
+	}
+	cout << "\n";
+}
+int32_t main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif 
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case #" << i << ": ";
+        Solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
+    return 0;
+}
